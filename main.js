a0076e33ceb8c1d8ad84c3494d6b7c75dbaf93ff
Dynamic05Dragon <94734201+Dynamic05Dragon@users.noreply.github.com>
2024-01-14 11:50:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,42 @@
-random_number=Math.floor((Math.random()*quick_draw_data_set.length)+1);
-console.log(quick_draw_data_set[random_number]);
-sketch=quick_draw_data_set[random_number];
-document.getElementById("sketch_name").innerHTML="Sketch to be Drawn"+sketch;
-timer_counter=0;
-timer_check="";
-drawn_sketch="";
-answer_holder="";
-score=0;
+function preload(){
+    classifier=ml5.imageClassifier('DoodleNet');
+
+}
+
+function setup(){
+    canvas=createCanvas(400,400);
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth=window.speechSynthesis;
+
+}
+
+function clearCanvas(){
+    background("white");
+}
+
+
+function draw(){
+    strokeWeight(7);
+    stroke("black");
+    if(mouseIsPressed){
+        line(pmouseX,pmouseY,mouseX,mouseY);
+    }
+    
+}
+
+function classifyCanvas(){
+    classifier.classify(canvas,gotResult)
+}
+
+function gotResult(error,results){
+     if(error){
+        console.error(error);
+     }
+     console.log(results);
+     document.getElementById("label").innerHTML="Label: "+results[0].label;
+     document.getElementById("confidence").innerHTML="Confidence: "+Math.round(results[0].confidence*100)+"%";
+     UtterThis=new SpeechSynthesisUtterance(results[0].label);
+     synth.speak(UtterThis);
+}
